ee55b45b0d081df76e3d58e401ae2b83b6bad045
Titogelo <titogelo@gmail.com>
2018-02-20 11:29:06 +0000

    making sure that me is the right user

--- auth/auth.controller.js
+++ auth/auth.controller.js
@@ -28,12 +28,12 @@ router.post('/register', function(req, res) {
 		surname : req.body.surname,
 		password : hashedPassword
 	},
-	function (err, user) {
+	function (err, teacher) {
 		if (err) 
-			return res.status(500).send("There was a problem registering the user.")
+			return res.status(500).send("There was a problem registering the teacher.")
 		// create a token
 		var token = jwt.sign({ 
-			id: user._id 
+			id: teacher._id 
 		}, config.secret, {
 			expiresIn: 86400 // expires in 24 hours
 		});
@@ -52,7 +52,12 @@ router.get('/me', function(req, res){
 		if (err)
 			return res.status(500).send({auth: false, message: 'Failed to authenticate accesToken.'})
 		
-		res.status(200).send(decoded);
+		Teacher.findOne(decoded.id, function (err, teacher) {
+		  if (err) return res.status(500).send("There was a problem finding the teacher.");
+		  if (!teacher) return res.status(404).send("No teacher found.");
+		  
+		  res.status(200).send(teacher);
+		});
 	})
 });
 
