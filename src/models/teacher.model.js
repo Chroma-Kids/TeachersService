186cbaf8e4cadff3e3863ad8c8b3a3656945a588
Titogelo <titogelo@gmail.com>
2018-02-20 10:21:35 +0000

    updated teachers model

--- src/models/teacher.model.js
+++ src/models/teacher.model.js
@@ -3,7 +3,25 @@ var mongoose = require('mongoose');
 var TeacherSchema = mongoose.Schema({
     name: String,
     surname: String,
-    
+    birthday: Date,
+    hourstotal: {
+    	type: Number
+    },
+    hoursweek: { 
+    	type: Number, 
+    	default: 40 
+    },
+    workshift: {
+        type: String,
+        enum : ['EARLY','NORMAL', 'LATE'],
+        default: 'EARLY'
+    },
+    type: {
+        type: String,
+        enum : ['FIXED','FLOATING'],
+        default: 'FIXED'
+    },
+    payload: mongoose.Schema.Types.Mixed
 }, {
     timestamps: true
 });
